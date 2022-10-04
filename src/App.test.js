19cdfec7fe9b896b3d8054318fe65d9de7b174ff
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-10-04 11:41:57 +0530

    test: tests added

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,8 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test ('will return undefined', () => {
+test('will return undefined', () => {
   const mock = jest.fn();
-
   let result = mock("Hello")
 
   expect(result).toBeUndefined()
@@ -17,7 +16,34 @@ test ('will return undefined', () => {
 })
 
 test('implementing mock functions', () => {
-  const mock =jest.fn(() => "Hello")
+  const mock = jest.fn(() => "Hello")
 
-  expect(mock("foo")).toBe("Hello")
+  expect(mock("World")).toBe("Hello")
+  expect(mock).toHaveBeenCalled()
+  expect(mock).toHaveBeenCalledWith("World")
+})
+
+test('mock implementation', () => {
+  const mock = jest.fn().mockImplementation(() => "Hello")
+
+  expect(mock("World")).toBe("Hello")
+  expect(mock).toHaveBeenCalled()
+  expect(mock).toHaveBeenCalledWith("World")
+})
+
+test('mock should return value', () => {
+  const mock = jest.fn()
+  mock.mockReturnValue("Hello")
+
+  expect(mock("World")).toBe("Hello")
+  expect(mock).toHaveBeenCalled()
+})
+
+test('mock should resolve promise', () => {
+  const mock = jest.fn()
+  mock.mockResolvedValue("Hello");
+
+  expect(mock("World")).resolves.toBe("Hello")
+  expect(mock).toHaveBeenCalledWith("World")
+
 })
